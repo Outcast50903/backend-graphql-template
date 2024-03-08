53d32dca1c19fa4ff4b6b054a1adfe67674a64c6
Diego Garcia <dgarciavallejos@gmail.com>
2024-03-08 13:46:54 -0300

    Add Apollo GraphQL support and modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,36 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault,
+} from 'apollo-server-core';
+import { join } from 'path';
+
+import { DrizzleModule } from './drizzle/drizzle.module';
+import { HelloModule } from './hello/hello.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [
+        process.env.NODE_ENV === 'production'
+          ? ApolloServerPluginLandingPageProductionDefault({
+              graphRef: 'my-graph-id@my-graph-variant',
+              footer: false,
+            })
+          : (ApolloServerPluginLandingPageLocalDefault({
+              footer: false,
+            }) as unknown),
+      ],
+    }),
+    DrizzleModule,
+    HelloModule,
+  ],
 })
 export class AppModule {}
