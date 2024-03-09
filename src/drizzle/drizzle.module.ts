d3417f237277c0cce8963b538631364aef163059
Diego Garcia <dgarciavallejos@gmail.com>
2024-03-09 20:03:07 -0300

    Remove Drizzle module and provider

--- src/drizzle/drizzle.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-
-import { DrizzleProvider } from './drizzle.provider';
-
-@Module({
-  providers: [...DrizzleProvider, ConfigService],
-})
-export class DrizzleModule {}
