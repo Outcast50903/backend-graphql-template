c23c220f8887b1fad1ecb80ebc0e657994c0ce4b
Diego Garcia <dgarciavallejos@gmail.com>
2024-03-10 15:27:20 -0300

    Add seed file for user creation

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import { randomUUID } from 'crypto';
+
+const prisma = new PrismaClient();
+
+const main = async () =>
+  await prisma.user.upsert({
+    where: { id: randomUUID() },
+    update: {},
+    create: {
+      email: process.env.ADMIN_EMAIL,
+      password: bcrypt.hashSync(process.env.ADMIN_PASSWORD, 10),
+      fullName: 'Admin',
+      isEnabled: true,
+      roles: ['ADMIN'],
+      createdAt: new Date(),
+    },
+  });
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
