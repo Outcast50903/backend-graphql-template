9c34715d896e30f433f38d0868b6104da90dac05
Diego Garcia <dgarciavallejos@gmail.com>
2024-03-12 15:19:17 -0300

    Update package.json and main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,43 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import csrf from 'csurf';
+import * as cookieParser from 'cookie-parser';
 import helmet from 'helmet';
+import * as csurf from 'tiny-csrf';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(csrf({ cookie: true }));
-  app.use(helmet());
+  app.use(cookieParser(process.env.COOKIE_KEY));
+  app.use(
+    csurf(
+      process.env.CSRF_SECRET,
+      ['POST'],
+      ['/api', /\/api\.*/i, '/graphql', /\/graphql\.*/i],
+      [],
+    ),
+  );
+  app.use(
+    helmet({
+      crossOriginEmbedderPolicy: false,
+      contentSecurityPolicy: {
+        directives: {
+          imgSrc: [
+            `'self'`,
+            'data:',
+            'apollo-server-landing-page.cdn.apollographql.com',
+          ],
+          scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+          manifestSrc: [
+            `'self'`,
+            'apollo-server-landing-page.cdn.apollographql.com',
+          ],
+          frameSrc: [`'self'`, 'sandbox.embed.apollographql.com'],
+        },
+      },
+    }),
+  );
 
   app.enableCors();
   app.setGlobalPrefix('api');
