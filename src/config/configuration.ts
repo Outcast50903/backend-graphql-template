761d7a94cb5563b13f52123c6524f2e01df33469
Diego Garcia <dgarciavallejos@gmail.com>
2024-03-12 22:35:54 -0300

    Remove unused database configuration

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,14 +1,3 @@
-const uri = [
-  'postgres://',
-  process.env.POSTGRES_USER,
-  ':',
-  process.env.POSTGRES_PASSWORD,
-  '@db.localtest.me:5432/',
-  process.env.POSTGRES_DB,
-  '?sslmode=require',
-].join('');
-
 export default () => ({
-  database_url:
-    process.env.NODE_ENV === 'development' ? uri : process.env.DATABASE_URL,
+  jwt_secret: process.env.JWT_SECRET,
 });
