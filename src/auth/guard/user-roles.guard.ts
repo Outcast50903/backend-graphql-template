7c0073622e9637c78dae90232cff8181307c82da
Diego Garcia <dgarciavallejos@gmail.com>
2024-03-13 00:09:14 -0300

    Add user roles guard for authorization

---
+++ src/auth/guard/user-roles.guard.ts
@@ -0,0 +1,49 @@
+import { ForbiddenError } from '@nestjs/apollo';
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { Prisma } from '@prisma/client';
+import { Observable } from 'rxjs';
+
+import { DefaultActions } from '@/common/enums';
+
+import { hasAbility } from '../../common/helpers/roles';
+import { META_ACTION } from '../decorators';
+import { META_RESOURCE } from '../decorators/resourse-protected.decorator';
+
+@Injectable()
+export class UserRolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const resource = this.reflector.get<Prisma.ModelName>(
+      META_RESOURCE,
+      context.getHandler(),
+    );
+
+    const action = this.reflector.get<DefaultActions>(
+      META_ACTION,
+      context.getHandler(),
+    );
+
+    if (!action) return false;
+    if (action.includes('read')) return true;
+
+    const user = GqlExecutionContext.create(context).getContext().req.user;
+    if (!user) throw new BadRequestException('User not found');
+
+    if (!hasAbility(user.roles, resource, action))
+      throw new ForbiddenError(
+        'Do not have permission to access this resource',
+      );
+
+    return true;
+  }
+}
