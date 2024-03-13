6fc3d339c9053dbfa38e4c3db07925f4aaf2f3ad
Diego Garcia <dgarciavallejos@gmail.com>
2024-03-13 00:11:04 -0300

    Add default actions enum

---
+++ src/common/enums/actions.enum.ts
@@ -0,0 +1,9 @@
+export enum DefaultActions {
+  read = 'read',
+  readMany = 'readMany',
+  aggregate = 'aggregate',
+  create = 'create',
+  update = 'update',
+  delete = 'delete',
+  manage = 'manage',
+}
