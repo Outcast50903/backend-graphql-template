e028b261bcc72f970b76fb8a7ef8e38e01831228
Diego Garcia <dgarciavallejos@gmail.com>
2024-03-14 18:58:54 -0300

    Update bcrypt import to bcryptjs

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 import { SingUpInput } from '@/auth/dto/signup.inputs';
 import { handleErrors } from '@/helpers';
